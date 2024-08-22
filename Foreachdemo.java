4de550b7c1b37cfebe05084180e6e2b497588442
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-08-22 12:35:32 +0530

    Create Foreachdemo.java

---
+++ Foreachdemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Foreachdemo
+{
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        for(int i:arr)
+        {
+            System.out.println(i+"");
+        }
+    }
+}
+ 
