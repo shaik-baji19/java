7965e30914ca8668efe9b751321f93c7f0b52290
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-06 15:01:22 +0530

    Create  ClassDemo.java

---
+++ ClassDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ class ClassDemo
+{
+	static String rollno;
+	static double avg;
+	public static void main(String args[])
+	{
+		rollno="23K61A05G8";
+		avg=91.5
+		System.out.println("Roll no is "+rollno+"\nAverage is "+avg);
+	}
+}
