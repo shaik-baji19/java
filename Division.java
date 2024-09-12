d34b1056a751e5f38d790c989e71cbba8370b660
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-12 09:44:07 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Division
+{
+public static void main(String args[])
+  {
+	int a=40;
+	int b=3;
+	int c=a/b;
+	System.out.println("Division of "+a+ "and " +b+ "is" +c);
+  }
+}
