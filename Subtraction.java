7314706b93b34b15ed7be9b6107da96d356ab390
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-12 09:41:56 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Subtraction
+{
+public static void main(String args[])
+  {
+	int a=50;
+	int b=10;
+	int c=a-b;
+	System.out.println("Difference of "+a+" and "+b+ "is" +c);
+  }
+}
