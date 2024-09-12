c07983483f151e9dc6ad7d9fa60e708a7ba41757
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-12 09:34:40 +0530

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class ArrayDemo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of the array : ");
+        int n=sc.nextInt();
+        int arr[]=new int[5];
+        System.out.println("Enter " +n+ " values into the array");
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The elements of the array are: ");
+        for(int i=0; i<n; i++)
+        {
+        System.out.print(arr[i]+" ");
+        }
+    }
+}
