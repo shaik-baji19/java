db609e5a6358d364b33d8e8b302caff9d6ade766
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-12 10:07:05 +0530

    Create Staticblockdemo.java

---
+++ Staticblockdemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Staticblockdemo
+{
+ public static void main(String args[])
+ {
+  System.out.println("Inside the main method");
+ }
+  static
+  {
+   System.out.println("Inside static block");
+  }
+}
+ 
