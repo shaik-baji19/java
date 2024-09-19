8affbc03c80407a93342ac2e0f9ea630b72bb2d0
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-09-19 10:50:49 +0530

    Update ClassDemo.java

--- ClassDemo.java
+++ ClassDemo.java
@@ -6,7 +6,7 @@ import java.io.*;
 	public static void main(String args[])
 	{
 		rollno="23K61A05G8";
-		avg=91.5
+		avg=91.5;
 		System.out.println("Roll no is "+rollno+"\nAverage is "+avg);
 	}
 }
