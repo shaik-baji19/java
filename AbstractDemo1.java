2cfba6fc0c63adbfb955719f79130705dfa3518e
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-10-17 11:22:46 +0530

    Create AbstractDemo1.java

---
+++ AbstractDemo1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+abstract class Base		
+{
+	abstract void fun();
+}
+class Derived extends Base
+{
+	void fun()
+	{
+		System.out.println("Derived fun is invoked");
+	}
+}
+class AbstractDemo1
+{
+	public static void main(String args[])
+	{
+		Derived d =  new Derived();
+		d.fun();
+		Base b = new Derived();
+		b.fun();
+	}
+}
