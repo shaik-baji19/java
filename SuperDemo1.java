1c4fd08170550d6b525509d4fea76696bc86546e
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-10-17 11:24:11 +0530

    Create SuperDemo1.java

---
+++ SuperDemo1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Animal{
+    String color="white";
+}
+class Dog extends Animal{
+    String color="Black";
+void printcolor(){
+        System.out.println("Dog color is "+color);
+    System.out.println("Animal color is " +super.color);
+}
+}
+class SuperDemo1{
+    public static void main(String args[]){
+        Dog d=new Dog();
+        d.printcolor();
+    }
+}
