ec9856f321e1f7f5f3ba39744905be72f6756d12
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-10-19 12:19:42 +0530

    TryCatch1.java

---
+++ TryCatch1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class TryCatch1
+{
+    public static void main(String args[])
+    {
+        int a=5,b=0,c;;
+        c=a/b;
+        System.out.println("The value is c is "+c);
+    }
+}
