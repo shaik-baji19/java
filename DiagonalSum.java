d45ed6cfa5e2e853e3a695b85401ed5254c6ae64
shaik-baji19 <151706280+shaik-baji19@users.noreply.github.com>
2024-11-07 12:06:13 +0530

    Create DiagonalSum.java

---
+++ DiagonalSum.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class DiagonalSum
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of the matrix: ");
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        System.out.println("Enter "+(r*c)+" elements into the array");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(i==j)
+                {
+                    sum=sum+arr[i][j];
+                }
+            }
+        }
+        System.out.println("Sum of principal diagonal elements is "+sum);
+    }
+}
